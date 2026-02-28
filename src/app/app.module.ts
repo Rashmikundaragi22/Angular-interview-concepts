97a4a5ea2ff40d484e8912962b81fd9e8ddb0346
Rashmikundaragi22 <rashmi@saranyu.in>
2026-02-28 21:11:39 +0530

    app file changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ParentComponent } from './Communication/parent/parent.component';
+import { ChildComponent } from './Communication/child/child.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
